c295ac91df51e76116951f40095c0a41deb5abca
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-03-01 23:50:14 +0600

    change product List

--- src/component/Products/ProductList.js
+++ src/component/Products/ProductList.js
@@ -15,7 +15,7 @@ const ProductList = ({product}) => {
                 <h4 class="lws-productName">{product.Pname}</h4>
                 <p class="lws-productCategory">{product.category}</p>
                 <div class="flex items-center justify-between pb-2">
-                    <p class="productPrice">BDT <span class="lws-price">400</span></p>
+                    <p class="productPrice">BDT <span class="lws-price">{product.price}</span></p>
                     <p class="productQuantity">Qun <span class="lws-quantity">{product.quantity}</span></p>
                 </div>
                 {cartId?<button class="lws-btnAddToCart" onClick={() => dispatch(addToCart(product))}>Add To Cart</button>:<p>All readu Added</p>}
